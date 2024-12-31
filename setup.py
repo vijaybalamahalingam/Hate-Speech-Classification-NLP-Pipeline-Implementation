fbe2b17e2c95ffc33cdb831b1f142fed4b625ea2
Vijay <vijay_5@Vijays-MacBook-Air.local>
2024-12-31 12:44:01 +0530

    feat: complete setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="ZoWE",
+    author_email="vijaybala.mahalingam@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
