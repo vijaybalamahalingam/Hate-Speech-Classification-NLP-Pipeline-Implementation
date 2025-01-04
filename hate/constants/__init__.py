c77d98714a10325441a23c9b657a7a52713b10bb
Vijay <vijay_5@Vijays-MacBook-Air.local>
2025-01-04 12:28:33 +0530

    feat: implement data transformation

--- hate/constants/__init__.py
+++ hate/constants/__init__.py
@@ -1,5 +1,7 @@
 import os
 from datetime import datetime
+
+
 # Common constants
 TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 ARTIFACTS_DIR = os.path.join("artifacts", TIMESTAMP)
@@ -7,7 +9,20 @@ BUCKET_NAME = 'hate-speech-2025'
 ZIP_FILE_NAME = 'Data.zip'
 LABEL = 'label'
 TWEET = 'tweet'
+
+
 # Data ingestion constants
 DATA_INGESTION_ARTIFACTS_DIR = "DataIngestionArtifacts"
 DATA_INGESTION_IMBALANCE_DATA_DIR = "imbalanced_data.csv"
 DATA_INGESTION_RAW_DATA_DIR = "raw_data.csv"
+
+
+# Data transformation constants 
+DATA_TRANSFORMATION_ARTIFACTS_DIR = 'DataTransformationArtifacts'
+TRANSFORMED_FILE_NAME = "final.csv"
+DATA_DIR = "data"
+ID = 'id'
+AXIS = 1
+INPLACE = True
+DROP_COLUMNS = ['Unnamed: 0','count','hate_speech','offensive_language','neither']
+CLASS = 'class'
--- hate/entity/artifact_entity.py
+++ hate/entity/artifact_entity.py
@@ -4,3 +4,8 @@ from dataclasses import dataclass
 class DataIngestionArtifacts:
     imbalance_data_file_path: str
     raw_data_file_path: str
+
+
+@dataclass
+class DataTransformationArtifacts:
+    transformed_data_path: str
